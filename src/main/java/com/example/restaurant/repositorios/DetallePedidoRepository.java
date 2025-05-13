2efe4daf5c9906b00c8d47f3534f4e2e19e3225c
raforamqutp <rafael.ramirez.utp@gmail.com>
2025-05-13 12:26:12 -0500

    Create DetallePedidoRepository.java
    Actualización para funcionalidad inicial v1.01

---
+++ src/main/java/com/example/restaurant/repositorios/DetallePedidoRepository.java
@@ -0,0 +1,7 @@
+package com.example.restaurant.repositorios;
+
+import com.example.restaurant.entidades.DetallePedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Integer> {
+}
