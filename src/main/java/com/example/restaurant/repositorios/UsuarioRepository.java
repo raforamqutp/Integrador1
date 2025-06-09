f7e4dfa12945eafbe458cab905fa527e0d63b1e9
Merllin23 <roche12369874@gmail.com>
2025-06-09 02:57:11 -0500

    Update UsuarioRepository.java

--- src/main/java/com/example/restaurant/repositorios/UsuarioRepository.java
+++ src/main/java/com/example/restaurant/repositorios/UsuarioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     // Query automática por nombre de método
-	Usuario findByNombreUsuarioAndContraseña(String nombreUsuario, String contraseña);
+	Usuario findByNombreUsuarioAndContrasena(String nombreUsuario, String contrasena);
+
 }
