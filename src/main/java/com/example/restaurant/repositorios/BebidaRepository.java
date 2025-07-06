b234cda0f423ee96361dd3753c4b1b942bad9324
RafaelR-UTP <rafael.ramirez.utp@gmail.com>
2025-07-06 16:15:54 -0500

    Código actualizado, corrección de inconsistencias entre versiones v4.x

--- src/main/java/com/example/restaurant/repositorios/BebidaRepository.java
+++ src/main/java/com/example/restaurant/repositorios/BebidaRepository.java
@@ -1,8 +1,11 @@
 package com.example.restaurant.repositorios;
 
 import com.example.restaurant.entidades.Bebida;
+import com.example.restaurant.entidades.TipoBebida;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface BebidaRepository extends JpaRepository<Bebida, Integer> {
     Bebida findByNombreBebida(String nombreBebida);
+    List<Bebida> findByTipoBebida(TipoBebida tipoBebida);
 }
--- src/main/java/com/example/restaurant/repositorios/ComidaRepository.java
+++ src/main/java/com/example/restaurant/repositorios/ComidaRepository.java
@@ -1,8 +1,12 @@
 package com.example.restaurant.repositorios;
 
 import com.example.restaurant.entidades.Comida;
+import com.example.restaurant.entidades.TipoComida;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ComidaRepository extends JpaRepository<Comida, Integer> {
     Comida findByNombreComida(String nombreComida);
+    List<Comida> findByTipoComida(TipoComida tipoComida);
 }
